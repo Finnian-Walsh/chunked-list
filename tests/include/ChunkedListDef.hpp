0a24b3f4ec04da7ae85aa6a4431310baafce2eb1
FinnianWalsh <finnian.walsh@edgbarroschool.co.uk>
2025-05-17 13:07:36 +0100

    Create BASIC_INITIALIZER_LIST macro for test definitions

--- tests/include/ChunkedListDef.hpp
+++ tests/include/ChunkedListDef.hpp
@@ -7,3 +7,5 @@ using chunked_list::ChunkedList;
 
 template<typename T, size_t N, template<typename> typename A>
 using CurrentList = ChunkedList<T, N, A>;
+
+#define BASIC_INITIALIZER_LIST IntegralInitializerList{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
